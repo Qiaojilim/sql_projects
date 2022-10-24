168a76359910314e6f808d57b23ea20c90f37463
qiaoji <qxu062@uottawa.ca>
2022-10-24 12:38:20 -0400

    update6

--- dvdrental/sql.py
+++ dvdrental/sql.py
@@ -8,6 +8,13 @@ Created on Tue Oct 18 09:36:38 2022
 ##interact with a database in PostgreSQl with Python.
 import psycopg2 as pg2
 
+import pandas as pd
+import pylab as pl
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+from matplotlib.patches import Patch
+
 """Making the initial connection: """
 secret = 'password'
 #conn = pg2.connect(database='dvdrental',user='postgres', password=secret)
@@ -69,26 +76,114 @@ and has an address ID lower than 500?
 
 cur.execute("SELECT * FROM customer LIMIT 1")    
 cur.description
-cur.execute("""
-            SELECT first_name, last_name FROM customer
-            WHERE first_name LIKE 'E%'
-            AND address_id < 500
-            ORDER BY customer_id DESC
-            LIMIT 1;
-            """)     
+sql =   """
+        SELECT first_name, last_name FROM customer
+        WHERE first_name LIKE 'E%'
+        AND address_id < 500
+        ORDER BY customer_id DESC
+        LIMIT 1;
+        """
+cur.execute(sql)     
 ans3 = cur.fetchall()
 
-conn.close()
+
+# "what range of a month, pay the most of money"
+# sql = """
+#       SELECT EXTRACT(DAY FROM payment_date) FROM payment
+#       ORDER BY EXTRACT(DAY FROM payment_date);
+#       """
+
+# cur.execute(sql)   
+
+# df = plt.read_sql(sql, conn)
+# colors = ['aqua', 'red', 'gold', 'royalblue', 'darkorange', 'green']
+# df.hist(bins=range(1, 32, 5), grid=False, color='#86bf91', sharex=True, zorder=2, rwidth=0.9)
+# pl.title("Histogram of payment frequencies from customer table", fontweight="bold")
+# pl.xlabel("Day")
+# pl.ylabel("Payment Times")
 
 
-command = 'SELECT * FROM payment'
-cur.execute(command)
+# 
+
+# cm = sns.color_palette("plasma",6) #Splitting the palette in the same amount of numbers bins
+    
+# # Plot histogram.
+# plot = sns.histplot(data=df, x='extract', kde=True, bins=range(1, 32, 5))
+
+# for bin_,i in zip(plot.patches,cm):
+#     bin_.set_facecolor(i)
+# plt.title("Histogram of payment frequencies from customer table", fontweight="bold")
+# plt.xlabel("Day")
+# plt.ylabel("Payment Times")
+
+# plt.show()
+
+
+# conn.close()
+
+
+
+sql = """
+   SELECT film.title film_title, category.name AS category_name, COUNT(rental.rental_date) rental_count
+   FROM film 
+   JOIN inventory 
+     ON inventory.film_id = film.film_id
+   JOIN rental 
+     ON inventory.inventory_id = rental.inventory_id
+   JOIN film_category 
+     ON film.film_id = film_category.film_id
+   JOIN category 
+     ON film_category.category_id = category.category_id
+ WHERE category.name IN ('Animation', 'Children', 'Classics', 'Comedy', 'Family')	
+ GROUP BY film_title, category_name
+ ORDER BY rental_count DESC
+ LIMIT 10;
+ 
+ 
+ 
+      """
+
+cur.execute(sql)   
+colors = [ 'red', 'gold', 'royalblue', 'darkorange', 'green']
+
+df = pd.read_sql(sql, conn)
+
+fig = plt.figure()
+# ax = plt.subplot(111)
+
+
+sns.set_style("dark")
+colours = {"Animation": "tab:blue", "Children": "tab:orange", "Classics":"tab:green", "Comedy":"tab:red", "Family":"tab:purple"}
+ax = df.plot(x = 'film_title', y='rental_count',kind="barh",
+        color=df['category_name'].replace(colours))
+
+
+
+for i, v in enumerate(df['rental_count']):
+    ax.text(v + 0.1, i + .05, str(v), color='grey', fontweight='bold')
+
+plt.title("Top 10 family films based on rental count")
+plt.xlabel("Rental Count")
+plt.ylabel("Film Title")
+plt.legend([Patch(facecolor=colours['Animation']),Patch(facecolor=colours['Children']),
+            Patch(facecolor=colours['Classics']), Patch(facecolor=colours['Comedy']),
+            Patch(facecolor=colours['Family'])], 
+           
+           
+           
+           ["Animation", "Children", "Classics", "Comedy", "Family"], bbox_to_anchor=(1.04, 1), borderaxespad=0,
+           )
+
+
+plt.savefig('foo.png', bbox_inches='tight', dpi=500)
+
+#######################################################
+#######################################################
 
 
 
 
-
-
-
-
-
+    
+    
+    
+    
